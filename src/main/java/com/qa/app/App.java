b13d2601086a254644777d2123dee66fafd5d031
ianc890 <x13114425@student.ncirl.ie>
2018-02-19 10:55:33 +0000

    Add Hashmap functionality

--- src/main/java/com/qa/app/App.java
+++ src/main/java/com/qa/app/App.java
@@ -3,8 +3,19 @@ package com.qa.app;
 public class App {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-
+		Account accountOne = new Account("John", "Doe", "C001");
+		Account accountTwo = new Account("Jane", "Doe", "C002");
+		Account accountThree = new Account("Mary", "Murphy", "C003");
+		
+		Service service = new Service();
+		service.addAccount(accountOne);
+		service.addAccount(accountTwo);
+		service.addAccount(accountThree);
+		
+		service.retrieveAccount(4);
+		service.retrieveAccount(1);
+		service.retrieveAccount(2);
+		service.retrieveAccount(3);
+		
 	}
-
 }
